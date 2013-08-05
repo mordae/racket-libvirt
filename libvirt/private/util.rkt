07ea61dac1bdb100a842c3615612fbd7dc5b46f6
Jan Dvořák <mordae@anilinux.org>
2013-08-05 16:38:02 +0200

    enhance bytes/string conversion utilities
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- libvirt/private/util.rkt
+++ libvirt/private/util.rkt
@@ -3,15 +3,18 @@
 ; Misc Utilities
 ;
 
-(require racket/contract)
+(require racket/contract
+         racket/string)
 
 (provide (all-defined-out))
 
 
-
 ;; String encoding shortcuts.
-(define ->b string->bytes/utf-8)
-(define ->s bytes->string/utf-8)
+(define (->b str)
+  (string->bytes/utf-8 str))
+
+(define (->s bstr)
+  (string-trim (bytes->string/utf-8 bstr) #rx"\u0000+"))
 
 
 ; vim:set ts=2 sw=2 et:
