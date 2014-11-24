c014811c7b74640d33be59d0e64c49cbb35b718b
Jan Dvořák <mordae@anilinux.org>
2014-11-24 11:30:28 +0100

    info.rkt: add xexpr-path dependency
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "scribble-lib" "xdr" "tandem" "misc1"))
+(define deps '("base" "scribble-lib" "xdr" "tandem" "misc1" "xexpr-path"))
 
 ; vim:set ts=2 sw=2 et:
